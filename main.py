7c38379e3ecfe45cfe4738a2f30bf273d635d990
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-02-16 00:58:33 -0500

    changed auth routes to actually use /auth

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ def root():
     return {"message": "wassup"}
 
 
-@app.post("/register", response_model=auth.AuthResponse)
+@app.post("/auth/register", response_model=auth.AuthResponse)
 async def registerUser(request: auth.Credentials):
     return await auth.registerUser(request.model_dump())
 
 
-@app.post("/login", response_model=auth.AuthResponse)
+@app.post("/auth/login", response_model=auth.AuthResponse)
 async def loginUser(request: auth.Credentials):
     return await auth.loginUser(request.model_dump())
 
 
-@app.post("/refresh", response_model=auth.AuthResponse)
+@app.post("/auth/refresh", response_model=auth.AuthResponse)
 async def refresh(request: auth.RefreshRequest):
     return await auth.refresh_token(request.refresh_token)
